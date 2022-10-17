46c311988237c9e2f45f7229ce6fd78af5e6680c
Davis <davis.jacob@hpe.com>
2022-10-17 17:51:09 +0000

    updating variable

--- scripts/airgap_deploy.sh
+++ scripts/airgap_deploy.sh
@@ -9,19 +9,19 @@ fi
 
 ###Pull the SLES image
 if [[ "$(crictl images -q registry.suse.com/bci/bci-base:latest 2> /dev/null)" == "" ]]; then
-   printf "\nPulling image from SUSE"
+   printf "\n\nPulling image from SUSE"
    crictl pull registry.suse.com/bci/bci-base:latest
 else
-   printf "\nImage already available locally"
+   printf "\n\nImage already available locally"
 fi
 
 ####Run SLES pod 
 
 if [ "$(kubectl get pods -n airgap| grep -i airgap-utility | awk '{print $3}')" != "Running" ]; then
-   printf "\nCreating new Airgap Utility pod"
+   printf "\n\nCreating new Airgap Utility pod"
    kubectl create -f airgap_pod.yaml -n airgap
 else
-   printf "\nAirgap Utility Pod already running in the NameSpace airgap"
+   printf "\n\nAirgap Utility Pod already running in the NameSpace airgap\n"
    exit 0
 fi
 
@@ -31,17 +31,16 @@ pod_name=$(kubectl get pods -n airgap |grep airgap-utility | awk '{print $1}')
 
 ### install Ansible and inside the pod
 
-printf "\nEnter the email ID for SUSEConnect Subscription:"
-read -p "\nEmail ID:" suse_email
+printf "\n\nEnter the email ID for SUSEConnect Subscription:"
+read -p "Email ID:" suse_email
 
-printf "\nEnter the registration code for SUSEConnect Subscription:"
-read -s -p "\nRegistration Code:" suse_pass
+printf "\n\nEnter the registration code for SUSEConnect Subscription:"
+read -s -p "Registration Code:" suse_pass
 
 ##Install packages
 kubectl exec -it pod/$pod_name -n airgap -- zypper install -y SUSEConnect
 kubectl exec -it pod/$pod_name -n airgap -- SUSEConnect -r $suse_pass -e $suse_email
-kubectl exec -it pod/$pod_name -n airgap -- SUSEConnect -p PackageHub/15.4/x86_64
-kubectl exec -it pod/$pod_name -n airgap -- zypper install -y ansible git sshpass wget vim awk skopeo
+kubectl exec -it pod/$pod_name -n airgap -- zypper install -y git sshpass wget vim awk skopeo
 kubectl exec -it pod/$pod_name -n airgap -- zypper addrepo 'http://download.opensuse.org/repositories/devel:/tools:/scm/15.3/devel:tools:scm.repo'
 kubectl exec -it pod/$pod_name -n airgap -- zypper install -y python3 python3-pip
 unset suse_pass suse_email  
